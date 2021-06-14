effad80326108b79c6e45333fda281659af5d4dd
KKozhinov <85897617+Alexeyi-r@users.noreply.github.com>
2021-06-14 21:49:03 +0300

    first commit

---
+++ RESTAURANT/RESTAURANT/MainWindow.xaml.cs
@@ -0,0 +1,269 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RESTAURANT
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class Product
+    {
+        public Uri ImagePreview
+        {
+            get
+            {
+                var imageName = Environment.CurrentDirectory + Image ?? "";
+                return System.IO.File.Exists(imageName) ? new Uri(imageName) : new Uri("pack://application:,,,/picture.png");
+            }
+        }
+    }
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+
+        private IEnumerable<Product> _DishList;
+
+        private int _CurrentPage = 1;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public int CurrentPage
+        {
+            get
+            {
+                return _CurrentPage;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    if ((_DishList.Count() % 10) == 0)
+                    {
+                        if (value <= _DishList.Count() / 10)
+                        {
+                            _CurrentPage = value;
+                            Invalidate();
+                        }
+                    }
+                    else
+                    {
+                        if (value <= (_DishList.Count() / 10) + 1)
+                        {
+                            _CurrentPage = value;
+                            Invalidate();
+                        }
+                    }
+                }
+            }
+        }
+
+        private string _SearchFilter = "";
+        public string SearchFilter
+        {
+            get
+            {
+                return _SearchFilter;
+            }
+            set
+            {
+                _SearchFilter = value;
+                if (PropertyChanged != null)
+                {
+                    PropertyChanged(this, new PropertyChangedEventArgs("DishList"));
+                }
+            }
+        }
+
+        private void SearchFilter_KeyUp(object sender, KeyEventArgs e)
+        {
+            SearchFilter = SearchFilterTextBox.Text;
+            Invalidate();
+        }
+
+        private bool _SortList = true;
+        public bool SortList
+        {
+            get
+            {
+                return _SortList;
+            }
+            set
+            {
+                _SortList = value;
+                if (PropertyChanged != null)
+                {
+                    PropertyChanged(this, new PropertyChangedEventArgs("DishList"));
+                }
+            }
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            SortList = (sender as RadioButton).Tag.ToString() == "1";
+        }
+
+        private void Invalidate()
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("DishList"));
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("CurrentPage"));
+        }
+
+        public IEnumerable<Product> DishList
+        {
+            get
+            {
+                var Result = _DishList;
+
+                if (_ProductTypeFilterValue > 0)
+                    Result = Result.Where(ai => ai.ProductTypeID == _ProductTypeFilterValue);
+
+                if (SearchFilter != "")
+                    Result = Result.Where(ai => ai.Title.IndexOf(SearchFilter, StringComparison.OrdinalIgnoreCase) >= 0);
+
+                if (SortList) Result = Result.OrderBy(ai => ai.MinCostForAgent);
+                else Result = Result.OrderByDescending(ai => ai.MinCostForAgent);
+
+                return Result.Skip((CurrentPage - 1) * 10).Take(10);
+
+
+
+
+            }
+            set
+            {
+                _DishList = value;
+
+                if (PropertyChanged != null)
+                {
+                    PropertyChanged(this, new PropertyChangedEventArgs("DishList"));
+                }
+            }
+        }
+
+        public IEnumerable<Product> PacientsList
+        {
+            get
+            {
+                var Result = _DishList;
+
+                if (SearchFilter != "")
+                    Result = Result.Where(ai => ai.Title.IndexOf(SearchFilter, StringComparison.OrdinalIgnoreCase) >= 0);
+
+                return Result;
+            }
+            set
+            {
+                _DishList = value;
+            }
+        }
+
+        public List<ProductType> DishTypeList { get; set; }
+
+        private int _ProductTypeFilterValue = 0;
+        public int ProductTypeFilterValue
+        {
+            get
+            {
+                return _ProductTypeFilterValue;
+            }
+            set
+            {
+                _ProductTypeFilterValue = value;
+                if (PropertyChanged != null)
+                {
+                    PropertyChanged(this, new PropertyChangedEventArgs("DishList"));
+                }
+            }
+        }
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+            DishList = Core.DB.Product.ToArray();
+            DishTypeList = Core.DB.ProductType.ToList();
+            DishTypeList.Insert(0, new ProductType { Title = "Все типы" });
+        }
+
+
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void PrevPage_Click(object sender, RoutedEventArgs e)
+        {
+            CurrentPage--;
+        }
+
+        private void NextPage_Click(object sender, RoutedEventArgs e)
+        {
+            CurrentPage++;
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            var addWindow = new AddWindow(new Product());
+            if (addWindow.ShowDialog() == true)
+            {
+                DishList = Core.DB.Product.ToArray();
+            }
+
+        }
+
+        private void RefreshButton_Click(object sender, RoutedEventArgs e)
+        {
+            var w = new MainWindow();
+            w.Show();
+
+            this.Close();
+        }
+
+        private void DeleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            var DeleteProduct = DishListView.SelectedItem as Product;
+            try
+            {
+                Core.DB.Product.Remove(DeleteProduct);
+                Core.DB.SaveChanges();
+
+                MessageBox.Show($"Удалено");
+
+                DishList = Core.DB.Product.ToArray();
+
+                if (PropertyChanged != null)
+                {
+                    PropertyChanged(this, new PropertyChangedEventArgs("DishList"));
+                }
+            }
+            catch { }
+
+        }
+
+        private void FilterTypeComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ProductTypeFilterValue = (FilterTypeComboBox.SelectedItem as ProductType).ID;
+        }
+
+        private void EditButton_Click(object sender, RoutedEventArgs e)
+        {
+            var EditTourWindow = new AddWindow(DishListView.SelectedItem as Product);
+            if (EditTourWindow.ShowDialog() == true)
+            {
+                DishList = Core.DB.Product.ToArray();
+            }
+        }
+    }
+}
